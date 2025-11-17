8ea1907f1610c979a9eb1aa3c6ecb6e74a28f198
TarekSaeed0 <naminode2020@gmail.com>
2025-11-17 21:55:14 +0200

    initial commit

---
+++ backend/src/main/java/com/github/oop_assignment_3/OopAssignment3Application.java
@@ -0,0 +1,13 @@
+package com.github.oop_assignment_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopAssignment3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopAssignment3Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/oop_assignment_3/OopAssignment3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.oop_assignment_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopAssignment3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

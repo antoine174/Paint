aeb541af3e6d52554fbbad93dab05e43bfcfbfac
TarekSaeed0 <naminode2020@gmail.com>
2025-11-19 15:58:46 +0200

    refactor: create an array to represent the available colors, and use a for loop in the html

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -3,10 +3,23 @@ import { Drawing } from '../../services/drawing';
 
 @Component({
   selector: 'app-header',
+  imports: [],
   templateUrl: './header.html',
-  styleUrls: ['./header.css'],
+  styleUrl: './header.css',
 })
 export class Header {
+  colors = [
+    { key: 'red', value: 'var(--color-red-500)' },
+    { key: 'blue', value: 'var(--color-blue-500)' },
+    { key: 'green', value: 'var(--color-green-500)' },
+    { key: 'yellow', value: 'var(--color-yellow-400)' },
+    { key: 'purple', value: 'var(--color-purple-500)' },
+    { key: 'pink', value: 'var(--color-pink-500)' },
+    { key: 'orange', value: 'var(--color-orange-500)' },
+    { key: 'cyan', value: 'var(--color-cyan-500)' },
+    { key: 'gray', value: 'var(--color-gray-800)' },
+    { key: 'white', value: 'var(--color-white)' },
+  ] as const;
 
   constructor(public drawing: Drawing) {}
 
